f85a17c5d8dbfd3bc70252b5f550540dd12c69fb
Your Name <you@example.com>
2024-02-02 11:15:25 -0500

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/rob521_1/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/rob521_1/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/rob521_1/catkin_ws/devel;/opt/ros/noetic' != '' else []
---
+++ build/turtlebot3_simulations/turtlebot3_gazebo/CMakeFiles/turtlebot3_drive.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/rob521_1/catkin_ws/devel/lib/turtlebot3_gazebo/turtlebot3_drive"
+  "/home/rob521_1/catkin_ws/devel/lib/turtlebot3_gazebo/turtlebot3_drive.pdb"
+  "CMakeFiles/turtlebot3_drive.dir/src/turtlebot3_drive.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/turtlebot3_drive.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
